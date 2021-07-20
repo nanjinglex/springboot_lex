2d01ad0b505318ec130cc731c6c13a3b760aa68b
刘二兴 <lexliu@tungee.com>
2021-07-20 18:47:26 +0800

    httpclient、FastHttpClient

--- src/test/java/com/example/springboottest/SpringbootTestApplicationTests.java
+++ src/test/java/com/example/springboottest/SpringbootTestApplicationTests.java
@@ -3,12 +3,18 @@ package com.example.springboottest;
 import com.example.springboottest.dao.MongoTestDao;
 import com.example.springboottest.pojo.MongoTest;
 import com.example.springboottest.service.UserService;
+import com.example.springboottest.util.HttpClientUtil;
+import io.itit.itf.okhttp.FastHttpClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class SpringbootTestApplicationTests {
@@ -47,6 +53,70 @@ class SpringbootTestApplicationTests {
     @Test
     void contextLoads() {
     }
+
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+
+
+    @Test
+    public void insert() {
+        List<Object[]> list = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++) {
+            //name age sex
+            String[] s = {"张三"+i,i+"","男"};
+            list.add(new Object[]{s[0],s[1],s[2]});
+        }
+
+
+
+        //执行批量插入操作
+        String sql = "INSERT INTO `student`(`name`,`age`,`sex`) VALUES (?,?,?)";
+        jdbcTemplate.batchUpdate(sql, list);
+
+    }
+
+
+    /**
+     * GET---无参测试
+     *
+     * @date 2018年7月13日 下午4:18:50
+     */
+    @Test
+    public void httpTest() {
+        String url="http://localhost:18080/verification/job/queryAllJobs";
+        try {
+            String resp= FastHttpClient.get().url(url).build().execute().string();
+            System.out.println(resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        String s = HttpClientUtil.doGet(url);
+        System.out.println(s);
+
+        //1.入参 jobId、ipPorts
+        String jobId = "201";
+        String[] ipPorts = {"localhost:18080","localhost:18081"};
+
+
+        Map<String, Object> param = new HashMap<>();
+        param.put("jobId","201");
+
+        String postUrl = null;
+        for (String s2 : ipPorts) {
+            postUrl = "http://"+s2+"/verification/elasticjob/pushJobToRegCenter";
+            String resullt = HttpClientUtil.doPost(postUrl, param);
+            System.out.println(resullt);
+        }
+
+
+
+    }
+
 
 
 
