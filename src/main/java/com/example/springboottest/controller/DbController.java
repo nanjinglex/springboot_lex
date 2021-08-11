bb4f777301f4380070ff17c6f6311aebd4787c6c
刘二兴 <lexliu@tungee.com>
2021-08-11 10:54:10 +0800

    本地缓存Map

--- src/main/java/com/example/springboottest/controller/DbController.java
+++ src/main/java/com/example/springboottest/controller/DbController.java
@@ -3,17 +3,16 @@ package com.example.springboottest.controller;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
 
 import com.example.springboottest.pojo.Foo;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,26 +21,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/mydb")
 public class DbController {
 
+    // 缓存
+    Map<String, Object> cache = new HashMap<>();
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/getUsers")
     public List<Map<String, Object>> getDbType(){
-        String sql = "select * from sys_user";
-        List<Map<String, Object>> list =  jdbcTemplate.queryForList(sql);
-        for (Map<String, Object> map : list) {
-            Set<Entry<String, Object>> entries = map.entrySet( );
-            if(entries != null) {
-                Iterator<Entry<String, Object>> iterator = entries.iterator( );
-                while(iterator.hasNext( )) {
-                    Entry<String, Object> entry =(Entry<String, Object>) iterator.next( );
-                    Object key = entry.getKey( );
-                    Object value = entry.getValue();
-                    System.out.println(key+":"+value);
-                }
-            }
+
+        List<Map<String, Object>> users = (List<Map<String, Object>>) cache.get("users");
+        if (StringUtils.isEmpty(users)) {
+            // 未命名缓存，查询数据库
+            String sql = "select * from sys_user";
+            users =  jdbcTemplate.queryForList(sql);
+            // 将查询得到的数据存入缓存
+            cache.put("users",users);
         }
-        return list;
+        for (Map<String, Object> map : users) {
+            map.keySet().forEach(key-> System.out.println(key+":"+map.get(key)));
+//            Set<Entry<String, Object>> entries = map.entrySet();
+//            if(entries != null) {
+//                Iterator<Entry<String, Object>> iterator = entries.iterator( );
+//                while(iterator.hasNext( )) {
+//                    Entry<String, Object> entry =(Entry<String, Object>) iterator.next( );
+//                    Object key = entry.getKey( );
+//                    Object value = entry.getValue();
+//                    System.out.println(key+":"+value);
+//                }
+//            }
+        }
+        return users;
     }
     @RequestMapping("/getFoos")
     public List<Foo> getFoos(){
