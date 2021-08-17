ce14746b1f7f237caaaf632f9170a0fcc9685a78
刘二兴 <lexliu@tungee.com>
2021-08-17 17:02:58 +0800

    装饰器模式（Decorator Pattern）

---
+++ src/main/java/com/example/springboottest/designpattern/decoratorpattern/Rectangle.java
@@ -0,0 +1,16 @@
+package com.example.springboottest.designpattern.decoratorpattern;
+
+/**
+ * @author lex
+ * @version 1.0.0
+ * @ClassName Rectangle.java
+ * @Description TODO
+ * @createTime 2021年08月17日 16:40:00
+ */
+public class Rectangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Shape: Rectangle");
+    }
+}
