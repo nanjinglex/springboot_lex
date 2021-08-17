f9ed9f86195a54ce6fe12c05444557f226f292c5
刘二兴 <lexliu@tungee.com>
2021-08-17 17:50:55 +0800

    代理模式（Proxy Pattern）

---
+++ src/main/java/com/example/springboottest/designpattern/proxypattern/Image.java
@@ -0,0 +1,12 @@
+package com.example.springboottest.designpattern.proxypattern;
+
+/**
+ * @author lex
+ * @version 1.0.0
+ * @ClassName Image.java
+ * @Description TODO
+ * @createTime 2021年08月17日 17:03:00
+ */
+public interface Image {
+    void display();
+}
