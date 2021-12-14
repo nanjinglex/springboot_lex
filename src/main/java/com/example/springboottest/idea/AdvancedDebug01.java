b90c3fcf27cb39552b5d1c744f7a3108a84b7333
liuerxing <648205982@qq.com>
2021-12-14 23:32:34 +0800

    feat: idea高级断点调试2

---
+++ src/main/java/com/example/springboottest/idea/AdvancedDebug01.java
@@ -0,0 +1,24 @@
+package com.example.springboottest.idea;
+
+public class AdvancedDebug01 {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 5;
+        printA();
+        System.out.println(a + b);
+
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("爱你" + i);
+        }
+
+    }
+
+    private static void printA() {
+        int c = 3;
+        int d = 4;
+        System.out.println("printA");
+    }
+
+}
--- src/main/java/com/example/springboottest/idea/AdvancedDebug02.java
+++ src/main/java/com/example/springboottest/idea/AdvancedDebug02.java
@@ -1,27 +1,10 @@
 package com.example.springboottest.idea;
 
-public class AdvancedFeat {
+public class AdvancedDebug02 {
     public static void main(String[] args) {
-
-        int a = 1;
-        int b = 5;
-        printA();
-        System.out.println(a + b);
-
-
-        for (int i = 0; i < 100; i++) {
-            System.out.println("爱你" + i);
-        }
-
         new Thread(new TaskTest1(), "线程1").start();
         new Thread(new TaskTest2(), "线程2").start();
         System.out.println("thread test");
-    }
-
-    private static void printA() {
-        int c = 3;
-        int d = 4;
-        System.out.println("printA");
     }
 
     static class TaskTest1 implements Runnable{
@@ -36,5 +19,4 @@ public class AdvancedFeat {
             System.out.println("task2执行了");
         }
     }
-
 }
