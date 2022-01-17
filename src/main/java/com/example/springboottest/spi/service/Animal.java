359e14c80c9c0197e7f51f1b06cf3127f6903eb6
刘二兴 <lexliu@tungee.com>
2022-01-17 16:57:23 +0800

    Feat: SPI test

---
+++ src/main/java/com/example/springboottest/spi/service/Animal.java
@@ -0,0 +1,12 @@
+package com.example.springboottest.spi.service;
+
+/**
+ * @author lex
+ * @version 1.0.0
+ * @ClassName Animal.java
+ * @Description TODO
+ * @createTime 2022年01月17日 16:48:00
+ */
+public interface Animal {
+    void bark();//
+}
