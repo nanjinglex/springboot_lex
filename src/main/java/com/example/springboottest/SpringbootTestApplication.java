7889a0f4a54e63d86376a961a464640d76a26734
刘二兴 <lexliu@tungee.com>
2022-01-21 17:57:54 +0800

    Feat: Springboot 添加ssl

--- src/main/java/com/example/springboottest/SpringbootTestApplication.java
+++ src/main/java/com/example/springboottest/SpringbootTestApplication.java
@@ -5,13 +5,19 @@ import com.example.springboottest.springboot2.Dog;
 import com.example.springboottest.springboot2.MyCongfig;
 import lombok.extern.slf4j.Slf4j;
 
+import org.apache.catalina.connector.Connector;
 import org.apache.log4j.helpers.LogLog;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.aspectj.weaver.Advice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
@@ -85,10 +91,10 @@ public class SpringbootTestApplication {
         Environment env = run.getEnvironment();
         log.info("\n----------------------------------------------------------\n\t" +
                         "Application '{}' is running! Access URLs:\n\t" +
-                        "Local: \t\thttp://localhost:{}\n\t" +
-                        "External: \thttp://{}:{}\n\t" +
-                        "Doc: \thttp://{}:{}/doc.html\n" +
-                        "Doc: \thttp://{}:{}/swagger-ui.html#\n" +
+                        "Local: \t\thttps://localhost:{}\n\t" +
+                        "External: \thttps://{}:{}\n\t" +
+                        "Doc: \thttps://{}:{}/doc.html\n" +
+                        "Doc: \thttps://{}:{}/swagger-ui.html#\n" +
                         "----------------------------------------------------------",
                 env.getProperty("spring.application.name"),
                 env.getProperty("server.port"),
@@ -98,11 +104,41 @@ public class SpringbootTestApplication {
                 env.getProperty("server.port"),
                 InetAddress.getLocalHost().getHostAddress(),
                 env.getProperty("server.port"));
+
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t" +
+                        "Doc: \thttp://{}:{}/doc.html\n" +
+                        "Doc: \thttp://{}:{}/swagger-ui.html#\n" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                8080,
+                InetAddress.getLocalHost().getHostAddress(),
+                8080,
+                InetAddress.getLocalHost().getHostAddress(),
+                8080,
+                InetAddress.getLocalHost().getHostAddress(),
+                8080);
     }
 
 //    @Bean
 //    public SpringUtil getSpringUtil() {
 //        return new SpringUtil();
 //    }
+
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }
 
 }
